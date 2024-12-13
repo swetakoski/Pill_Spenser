ea14ece49fc8053311bd8d79274d4e4a79e7387d
swetakoski <142903432+swetakoski@users.noreply.github.com>
2024-12-13 15:07:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <ctype.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
+#include <string.h>
 
 // GPIO Pin Assignments
 #define SW_0 9            // Dispense button GPIO pin
@@ -22,17 +21,16 @@
 // Timing Constants
 #define DEBOUNCE_DELAY_US 200000 // Debounce delay for button presses
 #define ERROR_BLINKS 5       // Number of LED blinks for errors
-#define LED_BLINK_DELAY 200 // LED sleep duration for blinks
-#define BUTTON_SLEEP 25
+#define ERROR_BLINK_DELAY 200 // LED blink duration for errors
+
 
 // uart
-#define UART uart1
+#define UARTID uart1
 #define BAUD_RATE 9600
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
-#define STRLEN 100
+#define STRLEN 200
 
-char response[STRLEN];
 
 // Stepper Motor Control Sequence
 const int half_step_sequence[8][4] = {
@@ -66,23 +64,27 @@ bool system_calibrated = false;
 // Function Prototypes
 void initialize_gpio(void);
 void switch_state(void);
-void blink_led(void);
-void led_controller(void);
 void idle_mode(void);
+void calibrating(void);
 void calibrate_motor(void);
 void activate_motor_step(uint sleep);
 void run_motor(int N);
 void dispensing_pills(void);
+void error_blinks(void);
 void piezo_interrupt(uint gpio, uint32_t events);
 void init_uart(void);
-void send_command(const uint8_t *command);
-bool receive_answer(uint wait_time);
+void send_command(const char *command);
+bool receive_answer(char *response);
 void send_at(void);
+void connect_lorawan(void);
+
+
 
 int main() {
     stdio_init_all();
     initialize_gpio();
-    //init_uart();
+    init_uart();
+    //send_at();
 
     while (1) {
         switch_state();
@@ -125,100 +127,77 @@ void initialize_gpio(void) {
 
 void switch_state(void) {
     switch (current_state) {
+        case STATE_CHECK_LORA_CONNECTION:
+            send_at();
+        break;
+        case STATE_CONNECT_LORA_TO_NETWORK:
+            connect_lorawan();
+        break;
         case STATE_WAIT:
+            printf("Entering idle mode. Press SW_2 to calibrate.\n");
             idle_mode();
             break;
         case STATE_CALIBRATION:
-            calibrate_motor();
+            calibrating();
             break;
         case STATE_DISPENSE_PILLS:
-            dispensing_pills();
+            if (!gpio_get(SW_0)) {
+                printf("Dispense button pressed. Dispensing pills...\n");
+                dispensing_pills();
+            }
             break;
-        /*
-        case STATE_CHECK_LORA_CONNECTION:
-            send_at();
-            break;
-        case STATE_CONNECT_LORA_TO_NETWORK:
-            //connect_lora_to_network();
-            break;
-            */
-    }
-}
-
-// led blinks
-void blink_led(void) {
-    gpio_put(LED_1, true);
-    sleep_ms(LED_BLINK_DELAY);
-    gpio_put(LED_1, false);
-    sleep_ms(LED_BLINK_DELAY);
-}
-
-// led controller
-void led_controller(void) {
-    while(gpio_get(SW_2) && gpio_get(SW_0)) {
-        if(system_calibrated == false) {
-            blink_led();
-        }else if(system_calibrated == true) {
-            gpio_put(LED_1, true);
-        }
     }
 }
 
 // Idle Mode
 void idle_mode(void) {
-    printf("Entering idle mode. ");
-    if(system_calibrated == false) {
-        printf("Press SW_2 to calibrate.\n");
-    }else {
-        printf("Press SW_0 to start pill dispenser or SW_2 to recalibrate.\n");
+    while(gpio_get(SW_2)) {
+        gpio_put(LED_1, true);
+        sleep_ms(200);
+        gpio_put(LED_1, false);
+        sleep_ms(200);
     }
-    led_controller();
     if(!gpio_get(SW_2)) {
-        sleep_ms(BUTTON_SLEEP);
+        sleep_ms(50);
         current_state = STATE_CALIBRATION;
     }
-    if(!gpio_get(SW_0) && system_calibrated == true) {
-        sleep_ms(BUTTON_SLEEP);
-        gpio_put(LED_1, false);
-        current_state = STATE_DISPENSE_PILLS;
-    }else if(!gpio_get(SW_0) && system_calibrated == false) {
-        sleep_ms(BUTTON_SLEEP);
-        printf("The motor has not been calibrated.\n");
-        while(!gpio_get(SW_0)) {
-            sleep_ms(BUTTON_SLEEP);
-        }
-    }
+}
+
+// Calibrating
+void calibrating(void) {
+    gpio_put(LED_1, true);
+    calibrate_motor();
+    gpio_put(LED_1, false);
 }
 
 // Calibrate Motor
 void calibrate_motor(void) {
-    gpio_put(LED_1, true);
     printf("Calibrating motor...\n");
     int total_steps = 0;
-    uint fork_hits = 0;
+    uint hit_counter = 0;
     bool previous_state = false;
     // find the first falling edge
     while (gpio_get(OPTO_FORK)) {
         activate_motor_step(6);
     }
     // run full 3 rounds and count steps
-    while(fork_hits < 3) {
-        activate_motor_step(4 - fork_hits);
+    while(hit_counter < 3) {
+        activate_motor_step(4 - hit_counter);
         total_steps++;
         if(!gpio_get(OPTO_FORK) && previous_state) {
-            fork_hits++;
+            hit_counter++;
         }
         previous_state = gpio_get(OPTO_FORK);
     }
     steps_per_revolution = total_steps / 3;
     // move motor a bit so dispenser wheel and dispenser base holes line up, value might depend on device
     for (int i = 0; i < 170; i++) {
-        activate_motor_step(1);
+        activate_motor_step(2);
     }
-    printf("Motor has been calibrated.\n");
+    printf("Motor has been calibrated. Press SW_0 to start dispensing pills.\n");
     //printf("%d\n", steps_per_revolution);
     system_calibrated = true;
-    current_state = STATE_WAIT;
+    current_state = STATE_DISPENSE_PILLS;
 }
 
 // Activate Motor Step
@@ -241,11 +220,9 @@ void run_motor(int N) {
 
 // Dispensing Pills
 void dispensing_pills(void) {
-    printf("Dispense button pressed. Dispensing pills...\n");
-    int sensor_triggers = 0;
-    int step_and_stop = 0;
+    int day_and_stop = 0;
     int steps_per_compartment = steps_per_revolution / 8; // Calculate steps per compartment
-    while(step_and_stop < 7){
+    while(day_and_stop < 7){
         sensor_triggered = false;
         // Rotate motor to the next compartment
         run_motor(steps_per_compartment);
@@ -254,28 +231,31 @@ void dispensing_pills(void) {
             tight_loop_contents();
         }
         if(sensor_triggered){
-            printf("Pill was released from the compartment:  #%d.\n", step_and_stop+1);
+            printf("Pill was released from the compartment:  #%d.\n", day_and_stop+1);
             sensor_triggered = false;
-            sensor_triggers++;
+            send_command("AT+MSG=\"Pill released\"\r\n");
+            sleep_ms(2000);
         }
         else{
-            printf("Pill wasn't dispensed from the compartment:  #%d.\n", step_and_stop+1);
-            for (int blink = 0; blink < 5; blink++) {
-                blink_led();
-            }
+            printf("Pill has not been dispensed from the compartment:  #%d.\n", day_and_stop+1);
+            error_blinks();
+            send_command("AT+MSG=\"Pill not released\"\r\n");
         }
-        sleep_ms(300);
+        sleep_ms(5000);
         //sleep_ms(30000);
-        step_and_stop++;
-    }
-    if(sensor_triggers == 7) {
-        printf("All pills have been dispensed, going back to idle state.\n");
-    }else if (sensor_triggers > 0 && sensor_triggers < 7) {
-        printf("%d pill(s) have been dispensed, going back to idle state.\n", sensor_triggers);
-    }else {
-        printf("No pills have been dispensed, going back to idle state.\n");
+        day_and_stop++;
     }
     current_state = STATE_WAIT;
+}
+
+// Handle Error
+void error_blinks(void) {
+    for (int blink = 0; blink < 5; blink++) {
+        gpio_put(LED_1, true);
+        sleep_ms(200);
+        gpio_put(LED_1, false);
+        sleep_ms(200);
+    }
 }
 
 // Interrupt Callback for Piezo Sensor
@@ -286,40 +266,103 @@ void piezo_interrupt(uint gpio, uint32_t events) {
 }
 
 
-
-// LORAWAN
-// most of this is straight from my lab4; some things need to be changed
-// TODO:
-// make it so response[] doesn't need to be a global variable
-// add the other commands
-// somehow possibly make it so you can send "AT+command" and then just replace +command with whatever
-// command you want to send except when you just send AT?
-
-/*
-#define COMMANDS_LIST_SIZE 5
-
-typedef struct {
-    const uint8_t *command;
-    const uint wait_time;
-} loracommand;
-
-// times are currently placeholders
-loracommand commands[COMMANDS_LIST_SIZE] = {
-    {"AT+MODE=LWOTAA\r\n", 1000},
-    {"AT+KEY=APPKEY,'9ba1d017ed21f75853ee6e855970241c'\r\n", 1000},
-    {"AT+CLASS=A\r\n", 1000},
-    {"AT+PORT=8\r\n", 1000},
-    {"AT+JOIN\r\n", 1000}
-};
-
-
-
-// initialize uart function
 void init_uart(void) {
-    uart_init(UART, BAUD_RATE);
+    uart_init(UARTID,BAUD_RATE);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
 }
+
+// function to send command to lora
+void send_command(const char *command) {
+    uart_write_blocking(UARTID, (uint8_t *)command, strlen(command));
+}
+
+// function to get and handle answer from lora
+bool receive_answer(char *response) {
+    bool answer_received = false;
+    int i = 0;
+
+    // empty the response string
+    //memset(response, 0, STRLEN);
+
+    // attempt reading uart within 500 ms
+    while(uart_is_readable_within_us(UARTID, 500000) && !answer_received) {
+        // read one character
+        char c = uart_getc(UARTID);
+        // if character is carriage return or newline, stop the string
+        if (c == '\r' || c == '\n') {
+            response[i] = '\0';
+            answer_received = true;
+            printf("%s\n", response);
+            if (c == '\r') {
+                uart_getc(UARTID);
+            }
+            // else keep adding the character to response string as long as it's not overflowing
+        } else {
+            if(i < STRLEN - 1) {
+                response[i++] = c;
+            }
+        }
+    }
+    return answer_received;
+}
+
+// function to send AT
+void send_at(void) {
+    printf("Checking connection to LoRa module...\n");
+    char response[STRLEN];
+
+    int attempts = 0;
+
+    // try to send AT max 5 times
+    while(attempts < 5) {
+        send_command("AT\r\n");
+        receive_answer(response);
+
+        // if there's the right answer stop the loop and move to state 3
+        if(strcmp(response, "+AT: OK") == 0) {
+            printf("Connected to LoRa module\n");
+            current_state=STATE_CONNECT_LORA_TO_NETWORK;
+            return;
+        }
+        attempts++;
+    }
+    printf("Module not responding\n");
+    current_state=STATE_WAIT;
+}
+
+void connect_lorawan() {
+    char response[STRLEN];
+    send_command("AT+MODE=LWOTAA\r\n");
+    receive_answer(response);
+    send_command("AT+KEY=APPKEY,\"b903ad771f90c92de88a8a1eecbb2404\"\r\n");
+    receive_answer(response);
+    send_command("AT+CLASS=A\r\n");
+    receive_answer(response);
+    send_command("AT+PORT=8\r\n");
+    receive_answer(response);
+    send_command("AT+JOIN\r\n");
+    receive_answer(response);
+    receive_answer(response);
+    sleep_ms(20000);
+    receive_answer(response);
+    receive_answer(response);
+    receive_answer(response);
+    send_command("AT+MSG=\"MESSAGE SENT\"\r\n");
+    receive_answer(response);
+
+    current_state=STATE_WAIT;
+}
+
+
+
+/*
+// NOTE: need to wait for response for every command; necessary wait time depends on the command -> change
+// uart_is_readable_within_us time depending on command?
+// most of this is straight from my lab4; some things need to be changed
+
+
 
 // function to send command to lora
 void send_command(const uint8_t *command) {
@@ -327,15 +370,21 @@ void send_command(const uint8_t *command) {
 }
 
 // function to get and handle answer from lora
-bool receive_answer(const uint wait_time) {
+bool receive_answer(void) {
     bool answer_received = false;
     int i = 0;
 
-   
-    while(uart_is_readable_within_us(UART, wait_time)) {
+    // empty the response string
+    memset(response, 0, STRLEN);
+
+    // attempt reading uart within 500 ms
+    while(uart_is_readable_within_us(UART, 500000)) {
+        // read one character
         char c = uart_getc(UART);
+        // if character is carriage return or newline, stop the string
         if (c == '\r' || c == '\n') {
             response[i] = '\0';
+            // else keep adding the character to response string as long as it's not overflowing
         } else {
             if(i < STRLEN - 1) {
                 response[i++] = c;
@@ -350,57 +399,31 @@ bool receive_answer(const uint wait_time) {
 void send_at(void) {
     printf("Checking connection to LoRa module...\n");
     const uint8_t send[] = "AT\r\n";
+
     int attempts = 0;
 
+    // try to send AT max 5 times
     while(attempts < 5) {
         send_command(send);
-        receive_answer(5000);
+        receive_answer();
+        // if there's the right answer stop the loop and move to state 3
         if(strcmp(response, "+AT: OK") == 0) {
-            printf("Connected to LoRa module.\n\n");
-            current_state = STATE_CONNECT_LORA_TO_NETWORK;
+            printf("Connected to LoRa module\n");
+            at_okay = true;
+            state = 3;
             return;
         }
         attempts++;
     }
-    printf("Module not responding.\n");
-    current_state = STATE_WAIT;
+    printf("Module not responding\n");
 }
 
 
-
-  void connect_lora_to_network()() {
-    uart_puts(UART_ID, "AT+JOIN\n");
-    printf("Joining LoRa network...\n");
-
-    // Define a timeout (e.g., 30 seconds)
-    uint32_t start_time = to_ms_since_boot(get_absolute_time());
-    uint32_t timeout_ms = 30000; // 30 seconds timeout
-    bool result = false;
-
-    while (strstr(lora_response, "+JOIN: Done") == NULL) {
-        readLoRaResponse();
-
-        if (strstr(lora_response, "+JOIN: NetID") == NULL) {
-            result = true;
-        }
-
-        // Check if timeout has occurred
-        if (to_ms_since_boot(get_absolute_time()) - start_time > timeout_ms) {
-            printf("Timeout while waiting for LoRa join response.\n");
-            return false; // Timeout, failed to join
-        }
-        
-}
-
-    }
-// Check if the join was successful
-    if (result) {
-        printf("Successfully joined LoRa network.\n");
-        return true;
-current_state = STATE_WAIT;
-    } else {
-        printf("Failed to join LoRa network. Response: %s\n", lora_response);
-        return false;
-    }
-
-}
+Commands:
+"+MODE=LWOTAA\r\n"
+"+KEY=APPKEY,"[hexadecimal key for your device]"\r\n"
+"+CLASS=A"\r\n"
+"+PORT=8"\r\n"
+"+JOIN"\r\n"
+"+MSG="[message here]"\r\n"
+*/
